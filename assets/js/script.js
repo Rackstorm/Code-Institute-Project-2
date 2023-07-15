131ee06dde8a421702e8ac11ffd63ab668bf697a
Rackstorm <>
2023-07-15 14:39:47 +0000

    Trying to fix js errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,51 @@
+const questions = [
+    {
+        question: "What acid is used in higher concentrations to clean concrete, industrial equipment, metal, and water?",
+        answers: [
+            { text: "Glycolic acid", correct: true },
+            { text: "Sodium hydroxide lye", correct: false },
+            { text: "White vinegar", correct: false },
+            { text: "Soap", correct: false }
+        ]
+    },
+    {
+        question: "What is lamb meat called?",
+        answers: [
+            { text: "Veil", correct: false },
+            { text: "Mutton", correct: true },
+            { text: "Beef", correct: false },
+            { text: "Steak", correct: false }
+        ]
+    },
+    {
+        question: "What is Dentophobia?",
+        answers: [
+            { text: "Fear of birds", correct: false },
+            { text: "Fear of dinosaurs", correct: false },
+            { text: "Fear of dentists", correct: true },
+            { text: "Fear of driving", correct: false }
+        ]
+    },
+    {
+        question: "Cynicism Is a word that originated from which language?",
+        answers: [
+            { text: "Latin", correct: false },
+            { text: "Italian", correct: false },
+            { text: "Spanish", correct: false },
+            { text: "Greek", correct: true }
+        ]
+    },
+    {
+        question: "Every minute you shed over how many dead skin cells?",
+        answers: [
+            { text: "30,000", correct: true },
+            { text: "10,000", correct: false },
+            { text: "76,000", correct: false },
+            { text: "5,000", correct: false }
+        ]
+    }
+];
 
-
-// variables
 
 const startButton = document.getElementById('start-button');
 const nextButton = document.getElementById('next-button');
@@ -100,7 +145,7 @@ function clearUp(element) {
 function gameTimer() {
     var sec = 15;
     var timer = setInterval(function () {
-        document.getElementById('gameTimer').innerHTML = '00:' + sec;
+        document.getElementById('timer').innerHTML = '00:' + sec;
         sec--;
         if (sec < 0) {
             clearInterval(timer);
