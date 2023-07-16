6751dc02d7d95e2ca6586aa44d6b066cb6967ade
Rackstorm <>
2023-07-16 07:17:54 +0000

    adding comments into js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ let currentScore = 0;
 startButton.addEventListener('click', startGame);
 nextButton.addEventListener('click', clearNextQuestion);
 
-// starting the game
+// asking the player to press start game - no questions/answer options will be available until clicking
 
 function startGame() {
     startButton.classList.add('hide');
@@ -30,35 +30,18 @@ function startGame() {
     nextQuestion();
 }
 
-function updateScore() {
-    currentScore++;
-    renderScore();
-}
 
-function renderScore() {
-    scoreValue.innerText = currentScore;
-}
-
-function timedOut() {
-
-
-}
-
+// preparing the next question by  shuffling the questions, resetting the currentQuestionIndex and starting the game timer
 function nextQuestion() {
     resetState();
     if (shuffledQuestions.length > currentQuestionIndex) {
         showQuestion(shuffledQuestions[currentQuestionIndex]);
         gameTimer();
-    } else {
-
     }
 }
 
-function clearNextQuestion() {
-    clearInterval(timer);
-    currentQuestionIndex++;
-    nextQuestion();
-}
+
+// loading the questions and options into the buttons on the page and activates the score update function when clicking
 
 function showQuestion(question) {
     theQuestions.innerText = question.question;
@@ -78,11 +61,7 @@ function showQuestion(question) {
     });
 }
 
-function resetState() {
-    clearUp(document.body);
-    clearTimerDisplay();
-    nextButton.classList.add('hide');
-}
+// if the chosen answer is correct the "Next Question" button will be visible and clickable to move on
 
 function selectAnswer(e) {
     const selectedButton = e.target;
@@ -98,6 +77,8 @@ function selectAnswer(e) {
     }
 }
 
+// showing the right and wrong answers after clicking - if correct answer the background will turn WebGL2RenderingContext, if incorrect it will turn red
+
 function settingStatus(element, correct) {
     clearUp(element);
     if (correct) {
@@ -107,15 +88,43 @@ function settingStatus(element, correct) {
     }
 }
 
+// clearing up the timer, incrementing the currentQuestionIndex and targets next question after clicking on your answer
+function clearNextQuestion() {
+    clearInterval(timer);
+    currentQuestionIndex++;
+    nextQuestion();
+}
+
+// clearing up the questions and answers as well as activating the clearing timer display function
+
+function resetState() {
+    clearUp(document.body);
+    clearTimerDisplay();
+    nextButton.classList.add('hide');
+}
+
+// clearing up the button background colors when moving on to the next question
+
 function clearUp(element) {
     element.classList.remove('correct');
     element.classList.remove('incorrect');
 }
 
-function clearTimerDisplay() {
-    document.getElementById('timer').innerHTML = '00:00';
+// updating the score continuesly throughout the game
+
+function updateScore() {
+    currentScore++;
+    renderScore();
 }
 
+// showing the amount of correct clicked answers continuesly throughout the game
+
+function renderScore() {
+    scoreValue.innerText = currentScore;
+}
+
+
+// the timer showing 15 seconds counting down - if running out of time an alert message and redirection will happen in timedOut() function
 
 function gameTimer() {
     let sec = 15;
@@ -129,4 +138,16 @@ function gameTimer() {
             clearNextQuestion();
         }
     }, 18000);
+}
+
+// resetting the timer display to "00:00" in the timer button after each answer is clicked
+
+function clearTimerDisplay() {
+    document.getElementById('timer').innerHTML = '00:00';
+}
+
+// alert and redirecting if/when timer runs out
+function timedOut() {
+
+
 }
