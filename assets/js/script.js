b22f82c713cf15c49fe4f3efab6ca526d40d0cd0
Rackstorm <>
2023-07-16 00:04:49 +0000

    Untangling some functions in order to fix errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,16 @@ const questionContainer = document.getElementById('question-container');
 const theQuestions = document.getElementById('question');
 const answerButtons = document.getElementById('answer-buttons');
 const scoreResult = document.getElementById('score');
+const scoreValue = document.getElementById('score-value');
 
 let shuffledQuestions;
 let currentQuestionIndex;
 let timer;
-
-
+let currentScore = 0; // Variable to track the current score
 
 // starting the game
-
 startButton.addEventListener('click', startGame);
-nextButton.addEventListener('click', () => {
-    currentQuestionIndex++;
-    nextQuestion();
-});
+nextButton.addEventListener('click', clearBeforeNextQuestion); // Update the event listener
 
 function startGame() {
     startButton.classList.add('hide');
@@ -25,28 +21,52 @@ function startGame() {
     currentQuestionIndex = 0;
     currentScore = 0;
     questionContainer.classList.remove('hide');
+    renderScore();
     nextQuestion();
-    gameTimer();
-    currentScore();
 }
 
-// write comment about managing set up of questions and visibility
+function updateScore() {
+    currentScore++;
+    renderScore();
+}
+
+function renderScore() {
+    scoreValue.innerText = currentScore;
+}
+
+function timedOut() {
+
+
+}
 
 function nextQuestion() {
-    clearInterval(timer);
     resetState();
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
+    if (shuffledQuestions.length > currentQuestionIndex) {
+        showQuestion(shuffledQuestions[currentQuestionIndex]);
+        gameTimer();
+    } else {
+
+    }
+}
+
+function clearBeforeNextQuestion() {
+    clearInterval(timer);
+    currentQuestionIndex++;
+    nextQuestion();
 }
 
 function showQuestion(question) {
     theQuestions.innerText = question.question;
+    answerButtons.innerHTML = ''; // Clear the answer buttons
     question.answers.forEach(answer => {
         const button = document.createElement('button');
         button.innerText = answer.text;
         button.classList.add('button');
         if (answer.correct) {
             button.dataset.correct = answer.correct;
-            currentScore++;
+            button.addEventListener('click', () => {
+                updateScore(); // Call updateScore() when a correct answer is selected
+            });
         }
         button.addEventListener('click', selectAnswer);
         answerButtons.appendChild(button);
@@ -55,13 +75,9 @@ function showQuestion(question) {
 
 function resetState() {
     clearUp(document.body);
+    clearTimerDisplay(); // Clear the timer display
     nextButton.classList.add('hide');
-    while (answerButtons.firstChild) {
-        answerButtons.removeChild(answerButtons.firstChild);
-    }
 }
-
-// write comment about answers and button visibility after choosing an answer
 
 function selectAnswer(e) {
     const selectedButton = e.target;
@@ -77,8 +93,6 @@ function selectAnswer(e) {
     }
 }
 
-// confirming whether answer is correct/incorrect and clearing up for the next question
-
 function settingStatus(element, correct) {
     clearUp(element);
     if (correct) {
@@ -93,21 +107,21 @@ function clearUp(element) {
     element.classList.remove('incorrect');
 }
 
-function currentScore() {
-
-};
+function clearTimerDisplay() {
+    document.getElementById('timer').innerHTML = '00:00'; // Reset the timer display
+}
 
 // a visible timer - the player has 15 sec for each question
 function gameTimer() {
-    var sec = 15;
-    timer = setInterval
-        (function () {
-            document.getElementById('timer').innerHTML = '00:' + sec;
-            sec--;
-            if (sec < 0) {
-                clearInterval(timer);
-                alert('You ran out of time! Try again..');
-                window.location = "index.html";
-            }
-        }, 1000);
-};
+    let sec = 15;
+    timer = setInterval(function () {
+        document.getElementById('timer').innerHTML = '00:' + (sec < 10 ? '0' + sec : sec);
+        sec--;
+        if (sec < 0) {
+            clearInterval(timer);
+            alert('You ran out of time!');
+            timedOut();
+            clearBeforeNextQuestion();
+        }
+    }, 18000);
+}
